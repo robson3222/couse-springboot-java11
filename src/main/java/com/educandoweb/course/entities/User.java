16a0931cf8fdb437f00895649752ad8f65653f01
robson3222 <robson3222@gmail.com>
2020-11-02 22:09:33 -0300

    h2 database, test profile, jpa

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -2,10 +2,21 @@ package com.educandoweb.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+
+@Entity
 public class User implements Serializable{
 	
 	
 	private static final long serialVersionUID = 1L;
+
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
 	private Long id;
 	private String name;
 	private String email;
